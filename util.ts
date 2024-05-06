c216765039d8677d3785ebe557e443f10343c6eb
Yoshiya Hinosawa <stibium121@gmail.com>
2024-05-07 00:14:34 +0900

    refactor: remove unused param in internal util (#10)

--- util.ts
+++ util.ts
@@ -229,8 +229,8 @@ export async function tryGetDenoConfig(
 
 export async function getWorkspaceModules(
   root: string,
-): Promise<[string, WorkspaceModule[]]> {
-  const [path, denoConfig] = await tryGetDenoConfig(root);
+): Promise<WorkspaceModule[]> {
+  const [_path, denoConfig] = await tryGetDenoConfig(root);
   const workspaces = denoConfig.workspaces;
 
   if (!Array.isArray(workspaces)) {
@@ -253,7 +253,7 @@ export async function getWorkspaceModules(
     }
     result.push({ ...workspaceConfig, [pathProp]: path });
   }
-  return [path, result];
+  return result;
 }
 
 export function getModule(module: string, modules: WorkspaceModule[]) {
@@ -281,9 +281,8 @@ export function checkModuleName(
 export async function applyVersionBump(
   summary: VersionBumpSummary,
   module: WorkspaceModule,
-  denoJson: string,
   dryRun = false,
-): Promise<[denoJson: string, VersionUpdateResult]> {
+): Promise<VersionUpdateResult> {
   const oldVersionStr = module.version;
   const oldVersion = parseSemVer(oldVersionStr);
   let diff = summary.version;
@@ -303,13 +302,13 @@ export async function applyVersionBump(
       `Currently this tool doesn't keep the comments in deno.jsonc files. Comments in the path "${path}" might be removed by this update.`,
     );
   }
-  return [denoJson, {
+  return {
     from: oldVersionStr,
     to: newVersionStr,
     diff,
     summary,
     path,
-  }];
+  };
 }
 
 export function createReleaseNote(
--- util_test.ts
+++ util_test.ts
@@ -658,7 +658,7 @@ Deno.test("tryGetDenoConfig()", async () => {
 });
 
 Deno.test("getWorkspaceModules()", async (t) => {
-  const [_, modules] = await getWorkspaceModules("testdata/basic");
+  const modules = await getWorkspaceModules("testdata/basic");
   assertEquals(modules.length, 5);
   assertEquals(modules.map((m) => m.name), [
     "@scope/foo",
@@ -671,7 +671,7 @@ Deno.test("getWorkspaceModules()", async (t) => {
 });
 
 Deno.test("getModule", async () => {
-  const [_, modules] = await getWorkspaceModules("testdata/basic");
+  const modules = await getWorkspaceModules("testdata/basic");
   const mod = getModule("foo", modules);
   assertExists(mod);
   assertObjectMatch(mod, {
@@ -681,43 +681,33 @@ Deno.test("getModule", async () => {
 });
 
 Deno.test("applyVersionBump() updates the version of the given module", async () => {
-  const [denoJson, versionUpdate] = await applyVersionBump(
+  const versionUpdate = await applyVersionBump(
     {
       module: "foo",
       version: "minor",
       commits: [],
     },
     { name: "@scope/foo", version: "1.0.0", [pathProp]: "foo/deno.json" },
-    `{}`,
     true,
   );
   assertEquals(versionUpdate.from, "1.0.0");
   assertEquals(versionUpdate.to, "1.1.0");
   assertEquals(versionUpdate.diff, "minor");
-  assertEquals(
-    denoJson,
-    `{}`,
-  );
 });
 
 Deno.test("applyVersionBump() consider major bump for 0.x version as minor bump", async () => {
-  const [denoJson, updateResult] = await applyVersionBump(
+  const updateResult = await applyVersionBump(
     {
       module: "foo",
       version: "major",
       commits: [],
     },
     { name: "@scope/foo", version: "0.0.0", [pathProp]: "foo/deno.jsonc" },
-    `{}`,
     true,
   );
   assertEquals(updateResult.from, "0.0.0");
   assertEquals(updateResult.to, "0.1.0");
   assertEquals(updateResult.diff, "minor");
-  assertEquals(
-    denoJson,
-    `{}`,
-  );
 });
 
 async function createVersionUpdateResults(
@@ -732,10 +722,9 @@ async function createVersionUpdateResults(
   ).filter(Boolean) as Diagnostic[];
   const updates = [];
   for (const summary of summaries) {
-    const [_denoJson, versionUpdate] = await applyVersionBump(
+    const versionUpdate = await applyVersionBump(
       summary,
       getModule(summary.module, modules)!,
-      "",
       true,
     );
     updates.push(versionUpdate);
@@ -744,7 +733,7 @@ async function createVersionUpdateResults(
 }
 
 Deno.test("createReleaseNote()", async (t) => {
-  const [_, modules] = await getWorkspaceModules("testdata/std_mock");
+  const modules = await getWorkspaceModules("testdata/std_mock");
   const [updates, _diagnostics] = await createVersionUpdateResults(
     exampleVersionBumps,
     modules,
@@ -753,7 +742,7 @@ Deno.test("createReleaseNote()", async (t) => {
 });
 
 Deno.test("createPrBody()", async (t) => {
-  const [_, modules] = await getWorkspaceModules("testdata/std_mock");
+  const modules = await getWorkspaceModules("testdata/std_mock");
   const [updates, diagnostics] = await createVersionUpdateResults(
     exampleVersionBumps,
     modules,
